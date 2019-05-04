e42159963f6deb1d8f4d02a1526f752a6113cc01
Sunitha Nadigar <47208735+SunithaNadigar@users.noreply.github.com>
2019-05-04 12:41:13 +0530

    Commit Point 1

--- WelcomeProgram.java
+++ WelcomeProgram.java
@@ -5,5 +5,6 @@ public class PractisingGit
     public static void main( String[] args )
     {
         System.out.println( "Welcome to GitHub" );
+        System.out.println( "Commit Point 1" );
     }
 }
