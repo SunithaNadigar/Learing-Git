4442a71e7fa2184e5e44e63036175933e37accd1
Sunitha Nadigar <47208735+SunithaNadigar@users.noreply.github.com>
2019-05-04 22:18:13 +0530

    Resolving issue

--- WelcomeProgram.java
+++ WelcomeProgram.java
@@ -11,6 +11,7 @@ public class PractisingGit
         System.out.println( "on Ponies" );
         System.out.println( "Fork........." );
         System.out.println( "Solving Issue (Creating New Issue on Learning Git Repo)" );
+         System.out.println( "Solving Issue by hash#" );
         
     }
 }
